1c5c8f33241aa8da52c882ff9f2308bc6e5fb1d0
Horseman <2607990168@qq.com>
2024-05-20 02:17:26 +0800

    message 20: testing for layout + styling

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -25,23 +25,21 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
     def test_can_start_a_list_and_retrieve_it_later(self):
-    #张三听说有—个在线待办事工项的应用
-    #他去看了这个应用的首页
-        self.browser.get(self.live_server_url)#(1)他注意到网页的标题和头部都包含“To-Do"这个词
+
+        self.browser.get(self.live_server_url)
         self.assertIn( 'To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text 
         self.assertIn( 'To-Do', header_text)
-    #应用有—个输入待办事I项的文本输入框
-        inputbox = self.browser.find_element(By.ID,'id_new_item') #(1)
+    
+        inputbox = self.browser.find_element(By.ID,'id_new_item') 
         self.assertEqual(
         inputbox.get_attribute('placeholder'),
         'Enter a to-do item'
         )
-        #他在文本输入框中输入了“Buy flowers""
-        inputbox.send_keys( 'Buy flowers')#(2)
-        #他按了回年每建钱建建后,页面更新了
-        #待办事项表格中显示了“1:Buy flowers"
-        inputbox.send_keys(Keys.ENTER)#(3)time-sleep(1)#(4)
+        
+        inputbox.send_keys( 'Buy flowers')
+
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.wait_for_row_in_list_table('1: Buy flowers')
         
@@ -53,10 +51,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy flowers')
         self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
-        #self.fail('Finish the test!')
-        #页面再次更新,她的清单中显示了这两个待办事项
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        #张三新建一个待办事项清单
+
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy flowers')
@@ -86,3 +82,25 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy flowers', page_text)
         self.assertIn('Buy milk', page_text)
+    def test_layout_and_styling(self):
+        
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
