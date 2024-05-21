4b7dc958fcd2d1aa283ce2e8695ac13b1ccf804f
Horseman <2607990168@qq.com>
2024-05-21 14:11:43 +0800

    message 24: move static file and database outside of main souce tree

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 STATIC_URL = '/static/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
@@ -118,5 +119,5 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+STATIC_ROOT=os.path.join(BASE_DIR, '../static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
